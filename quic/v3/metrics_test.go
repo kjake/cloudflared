6a89a92a89ae87e12d694c0001ab23757909b7d9
kjake <kjake@users.noreply.github.com>
2025-05-03 07:33:47 -0400

    chore: baseline custom workflows & configs

--- quic/v3/metrics_test.go
+++
@@ -1,9 +0,0 @@
-package v3_test
-
-type noopMetrics struct{}
-
-func (noopMetrics) IncrementFlows()    {}
-func (noopMetrics) DecrementFlows()    {}
-func (noopMetrics) PayloadTooLarge()   {}
-func (noopMetrics) RetryFlowResponse() {}
-func (noopMetrics) MigrateFlow()       {}
